74de16ed825815854ec04d2cd24e9d5472c1354e
Coldy001 <coldpratik2001@gmail.com>
2020-10-28 14:58:34 +0530

    Second Commit 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 import "./App.scss";
 
 import Timer from "./components/Timer";
